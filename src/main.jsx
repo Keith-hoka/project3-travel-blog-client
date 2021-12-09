3498bb335dad20f217b8a901b86751016a3ddcae
Keith_hoka <keith_d_hau@hotmail.com>
2021-12-09 20:34:24 +1100

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+console.log(process.env.BASE_URL)
 
 ReactDOM.render(
   <React.StrictMode>
